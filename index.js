0bffc8066c0fd3a6d53ceda6242d242f70ab980b
Song Zhou <zhousong1993@gmail.com>
2018-06-10 18:53:11 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 
+const jsdom = require('jsdom')
+const {JSDOM} = jsdom
+
 const _post_path = path.join(process.cwd(), '_posts')
 const _index_path = path.join(process.cwd(), "index.html")
 
@@ -8,18 +11,37 @@ const _index_path = path.join(process.cwd(), "index.html")
 // let doc = parser.parseFromString(,"text/html")
 
 /// read index.html
-fs.readFile(_index_path, {encoding: 'utf-8'}, (error, data) => {
-})
+JSDOM.fromFile(_index_path, null).then(dom => {
+	const document = dom.window.document
+	const postsElement = document.getElementsByClassName('posts')[0]
 
-function post_files(dir, callback) {
+
+	/// get post files
 	fs.readdir(_post_path, (error, files) => {
-		files.forEach(file => {
-			// console.log(file)
+		files.filter(file => {
+			const ext = path.extname(file)
+			return ext === '.md'
+		}).forEach(file => {
+			/// list node
+			const postListNode= document.createElement("li")
+
+			/// a node
+			const postHrefNode = document.createElement("a")
+			postListNode.appendChild(postHrefNode)
+			const postHref = document.createAttribute("href")
+			postHref.value = 'posts/' + path.parse(file).name + '.html'
+			postHrefNode.setAttributeNode(postHref)
+
+			/// title
+			const title = document.createTextNode(path.basename(file))
+			postHrefNode.appendChild(title)
+
+			/// insert post node
+			postsElement.insertBefore(postListNode, null)
+
 		})
 	})
-}
+	
+	console.log(dom.serialize())
 
-
-function html_post(file_path) {
-
-}
+})
